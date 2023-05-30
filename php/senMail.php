827025e6262f0e43afb6ee8366d8b5a7311e4167
Nicolas Daunac <97985114+Farfadeli@users.noreply.github.com>
2023-05-30 06:11:16 +0200

    cahnge bug mail

--- php/senMail.php
+++ php/senMail.php
@@ -1,8 +1,9 @@
 <?php
     $entete  = 'MIME-Version: 1.0' . "\r\n";
     $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-    $entete .= 'Reply-to: ' . $_POST['mail'];
+    $entete .= 'From: ' . $_POST['mail'];
 
     $retour = mail('ndaunac@gmail.com', $_POST["object"], $_POST['content'], $entete);
 
+    header('Location: https://ndaunac.tech');
 ?>
